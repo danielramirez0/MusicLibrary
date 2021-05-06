ec46c3f038038e1667caa1a34b1c3802a7f9b0f5
Daniel Ramirez <notused@notused.net>
2021-05-06 08:30:35 -0700

    Working on moving table filtering and state to top level

--- src/components/Table/table.jsx
+++ src/components/Table/table.jsx
@@ -4,63 +4,29 @@ import SearchBar from "../SearchBar/searchBar";
 
 import "./table.css";
 
-class Table extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: props.music,
-      filteredData: props.music,
-      searchInput: "",
-    };
-  }
-
-  handleChange = (event) => {
-    this.setState({ searchInput: event.target.value }, () => {
-      this.globalSearch();
-    });
-  };
-
-  globalSearch = () => {
-    let { searchInput, data } = this.state;
-    let filteredData = data.filter((value) => {
-      return (
-        value.id.toString().includes(searchInput.toString()) ||
-        value.title.toLowerCase().includes(searchInput.toLowerCase()) ||
-        value.album.toLowerCase().includes(searchInput.toLowerCase()) ||
-        value.artist.toLowerCase().includes(searchInput.toLowerCase()) ||
-        value.genre.toLowerCase().includes(searchInput.toLowerCase()) ||
-        value.releaseDate.toString().toLowerCase().includes(searchInput.toLowerCase())
-      );
-    });
-    this.setState({ filteredData });
-  };
-
-  render() {
-    // const Table = (props) => {
-    return (
-      <>
-        <SearchBar inputTracking={(e) => this.handleChange(e)} />
-        <table className="table table-dark table-striped">
-          <thead>
-            <tr>
-              <th scope="col">ID</th>
-              <th scope="col">SONG</th>
-              <th scope="col">ALBUM</th>
-              <th scope="col">ARTIST</th>
-              <th scope="col">GENRE</th>
-              {/* <th scope="col" style={{ display: this.state.showStore ? "block" : "none" }}> */}
-              <th scope="col">RELEASE DATE</th>
-              <th scope="col">REMOVE RECORD</th>
-            </tr>
-          </thead>
-          <tbody>
-            <Row music={this.state.filteredData} deleteRecord={this.props.deleteRecord} />
-          </tbody>
-        </table>
-      </>
-    );
-  }
-  // };
-}
+const Table = (props) => {
+  return (
+    <>
+      <SearchBar inputTracking={(e) => props.handleChange(e)} />
+      <table className="table table-dark table-striped">
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">SONG</th>
+            <th scope="col">ALBUM</th>
+            <th scope="col">ARTIST</th>
+            <th scope="col">GENRE</th>
+            {/* <th scope="col" style={{ display: this.state.showStore ? "block" : "none" }}> */}
+            <th scope="col">RELEASE DATE</th>
+            <th scope="col">REMOVE RECORD</th>
+          </tr>
+        </thead>
+        <tbody>
+          <Row music={props.music} deleteRecord={props.deleteRecord} />
+        </tbody>
+      </table>
+    </>
+  );
+};
 
 export default Table;
--- src/components/app.jsx
+++ src/components/app.jsx
@@ -86,7 +86,10 @@ class App extends Component {
   async runPromise() {
     try {
       const response = await this.getMusicData(this.state.mainEndpoint);
-      this.setState(((this.state.rootData = response.data), (this.state.viewData = response.data)));
+      this.setState({
+        rootData: response.data,
+        viewData: response.data,
+      });
     } catch (error) {
       console.log(error);
     }
@@ -105,6 +108,27 @@ class App extends Component {
     this.state.showTable === true ? this.setState({ showTable: false }) : this.setState({});
   }
 
+  handleChange = (event) => {
+    this.setState({ searchInput: event.target.value }, () => {
+      this.globalSearch();
+    });
+  };
+
+  globalSearch = () => {
+    let { searchInput, data } = this.state;
+    let filteredData = data.filter((value) => {
+      return (
+        value.id.toString().includes(searchInput.toString()) ||
+        value.title.toLowerCase().includes(searchInput.toLowerCase()) ||
+        value.album.toLowerCase().includes(searchInput.toLowerCase()) ||
+        value.artist.toLowerCase().includes(searchInput.toLowerCase()) ||
+        value.genre.toLowerCase().includes(searchInput.toLowerCase()) ||
+        value.releaseDate.toString().toLowerCase().includes(searchInput.toLowerCase())
+      );
+    });
+    this.setState({ filteredData });
+  };
+
   render() {
     return (
       <div className="App">
@@ -112,7 +136,7 @@ class App extends Component {
         <TitleBar />
         <ButtonGroup toggleTable={(mode) => this.displayTable(mode)} toggleForm={(mode) => this.displayForm(mode)} resetUI={() => this.displayReset()} />
         {this.state.showForm === false ? null : <Form addMusicData={(record) => this.addMusicData(record)} />}
-        {this.state.showTable === false ? null : <Table music={this.state.viewData} deleteRecord={(record) => this.deleteMusicData(record)} />}
+        {this.state.showTable === false ? null : <Table music={this.state.viewData} deleteRecord={(record) => this.deleteMusicData(record)} handleChange={(e) => this.handleChange(e)} />}
       </div>
     );
   }
